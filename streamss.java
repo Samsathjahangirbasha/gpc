5189fd368d4acfc2d9d34fd90d380d1ce058c2e2
Samsath-J <samsath0108@gmail.com>
2025-02-19 22:28:17 +0530

    edited

--- streamss.java
+++ streamss.java
@@ -1,24 +1,25 @@
-package gpc;
+//package gpc;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.stream.Stream;
 public class streamss {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        System.out.println("ENTER -1 TO STOP THE LOOP");
-        while(true){
-            int num=sc.nextInt();
-            if(num==-1){
-                break;
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> arr = new ArrayList<Integer>();
+            System.out.println("ENTER -1 TO STOP THE LOOP");
+            while(true){
+                int num=sc.nextInt();
+                if(num==-1){
+                    break;
+                }
+                else{
+                    arr.add(num);
+                }
             }
-            else{
-                arr.add(num);
-            }
+            Stream<Integer> s=arr.stream()
+                    .filter(n->n%2==0);
+            s.forEach(n->System.out.println(n));
         }
-        Stream<Integer> s=arr.stream()
-                          .filter(n->n%2==0);
-        s.forEach(n->System.out.println(n));
     }
     
 }
